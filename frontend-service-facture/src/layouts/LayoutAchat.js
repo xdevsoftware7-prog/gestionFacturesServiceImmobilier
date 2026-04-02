6a1944b2e22e48a7b0a7409194931ec685226548
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-02 15:13:44 +0100

    Appel de composant NavbarAchat dans le composant LayoutAchat

--- frontend-service-facture/src/layouts/LayoutAchat.js
+++ frontend-service-facture/src/layouts/LayoutAchat.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom';
+import NavbarAchat from '../components/NavbarAchat';
 
 
 export default function LayoutAchat() {
   return (
     <div>
-        <nav style={{ background:'#2c3e50', color:'white', padding:'10px' }}>
-            <h3>Espace Achat (Fournisseurs)</h3>
-            <Link to="/achat/factures" style={{color:'white', marginRight:'10px' }}>Factures</Link>
-            <Link to="/achat/fournisseurs" style={{color:'white' }}>Fournisseurs</Link>
-        </nav>
+        <NavbarAchat/>
         <div style={{ padding:'20px' }}>
             <Outlet/>
         </div>
