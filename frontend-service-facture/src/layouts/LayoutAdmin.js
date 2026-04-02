59c5a740df38995fb3e4acdab78ed198599ad8e3
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-02 13:02:51 +0100

    Création de layout Admin

---
+++ frontend-service-facture/src/layouts/LayoutAdmin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+export default function LayoutAdmin() {
+  return (
+    <div style={{ textAlign:'center',marginTop:'100px' }}>
+        <p>Bienvenu Mr Ayoub</p>
+        <p>Les stats vont arriver plus tot</p>
+    </div>
+  )
+}
