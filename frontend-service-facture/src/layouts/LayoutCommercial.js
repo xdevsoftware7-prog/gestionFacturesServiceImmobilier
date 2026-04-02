a1ae5a433c271b0d3b940079aa462ab0923d4e76
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-02 15:18:35 +0100

    Appel de composant NavbarCommcercial dans le composant LayoutCommercial

--- frontend-service-facture/src/layouts/LayoutCommercial.js
+++ frontend-service-facture/src/layouts/LayoutCommercial.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import NavbarCommercial from '../components/NavbarCommercial';
 
 export default function LayoutCommercial() {
   return (
     <div>
-        <nav style={{ background:'#27ae60',color:'white',padding:'10px' }}>
-            <h3>ESPACE COMMERCIAL (Clients) </h3>
-            <Link to="/commercial/factures" style={{ color:'white',marginRight:'10px' }}>Ventes</Link>
-            <Link to="/commercial/clients" style={{ color:'white' }}>Clients</Link>
-        </nav>
+        <NavbarCommercial/>
         <div style={{ padding:'20px' }}>
             <Outlet/>
         </div>
