4d4553ab31f2defb0f68092e075cfe2434be06e1
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-02 14:58:06 +0100

    Fix bug: Correction de url axios. Ajout d'une slash manquante

--- frontend-service-facture/src/api/axiosConfig.js
+++ frontend-service-facture/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000" // url de mon gateway
+    baseURL: "http://localhost:3000/" // url de mon gateway
 });
 
 // Injection de token dans chaque request
