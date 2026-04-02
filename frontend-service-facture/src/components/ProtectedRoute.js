2572ca4d49824a6e886bf5f4ea093cb99aed17e2
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-02 11:51:31 +0100

    Création de composant 'ProtectedRoute' pour protéger les accès aux différents composant selon les roles et le service

---
+++ frontend-service-facture/src/components/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const ProtectedRoute = ({children, allowedService}) =>{
+    const { isAuthenticated, userService, userRole } = useSelector((state)=>state.auth);
+
+    // si user n'est pas connecter (token non valide,)
+    if(!isAuthenticated){
+        return <Navigate to="/login" />;
+    }
+
+    // si user n'est pas admin ou n'appartient pas au service convenable
+    if(userRole !== 'admin' && userService !== allowedService){
+        return <Navigate to="/unauthrized" />;
+    }
+    return children;
+}
+
+export default ProtectedRoute;
