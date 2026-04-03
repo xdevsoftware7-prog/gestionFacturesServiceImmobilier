fae8afbcefa986ec10d2fa4584e69f3b5dde8bfa
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-03 19:13:04 +0100

    Appel de FactureForm pour les fournisseurs

---
+++ frontend-service-facture/src/pages/achat/FournisseurFacture.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FactureForm from '../../components/shared/FactureForm';
+
+export default function FournisseurFacture() {
+  return (
+    <FactureForm type={"fournisseurs"}></FactureForm>
+  )
+}
